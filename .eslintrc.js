a88f7ce1af7f523fd5802b5f3ef8505801e8c2f3
bqgama <bqgama@gmail.com>
2022-10-26 18:40:59 -0300

    changed as requeted

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es2021: true,
+    jest: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
-    'prettier'
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-    'react-hooks'
-  ],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': 0,
-    'import/prefer-default-export': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
   },
 };
