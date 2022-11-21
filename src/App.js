4b10a58b8532d1863c682239e2b74ee166d00a91
bqgama <bqgama@gmail.com>
2022-11-20 23:48:53 -0300

    added history and toastify; edited routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
+import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes />
-        <GlobalStyle />
-      </BrowserRouter>
-    </>
+    <Router history={history}>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+      <ToastContainer autoClose={3000} className="toast-container" />
+    </Router>
   );
 }
 
